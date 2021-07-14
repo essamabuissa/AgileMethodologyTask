73a3eb3f3071cf1fbd0fdd7f41233182a7bc2cd3
Essam Abuissa <essamabuissa@hotmail.com>
2021-07-14 22:33:50 +0300

    style fixes on the card

--- src/Screens/Books/BookCard.tsx
+++ src/Screens/Books/BookCard.tsx
@@ -1,47 +1,41 @@
 import React from "react";
 import { TouchableOpacity, Text, StyleSheet, Image, View } from "react-native";
-
+import { BookCardProps } from "../../Common/Types";
 const styles = StyleSheet.create({
   card: {
-    borderWidth: 1,
-    width: "50%",
-    height: "25%",
+    borderRadius: 5,
+    backgroundColor: "white",
+    marginHorizontal: 20,
+    marginVertical: 5,
+    shadowOpacity: 0.1,
   },
   cardBody: {
     paddingHorizontal: 5,
   },
-  image: {
-    height: "50%",
-    borderWidth: 0,
+
+  numberOfPages: {
+    fontWeight: "bold",
+    fontSize: 10,
   },
   title: {
     fontWeight: "bold",
     fontSize: 20,
   },
-  numberOfPages: {
+  subTitle: {
+    fontSize: 12,
     fontWeight: "bold",
   },
 });
 
-const BookCard = ({ onPress, book, thumbnail }) => {
+const BookCard = ({ onPress, book, thumbnail }: BookCardProps) => {
   console.log(book, "card");
   return (
     <TouchableOpacity onPress={onPress} style={styles.card}>
-      <Image
-        style={styles.image}
-        source={{
-          url: thumbnail,
-        }}
-      />
       <View style={styles.cardBody}>
         <Text style={styles.title}>{book?.title}</Text>
-        <Text>{book?.subtitle}</Text>
-        <Text style={styles.numberOfPages}>
-          Number of pages:
-          <Text style={{ fontWeight: "normal", paddingLeft: 20 }}>
-            {book?.number_of_pages}
-          </Text>
-        </Text>
+        <Text style={styles.subTitle}>Published Date {book?.publish_date}</Text>
+
+        <Text style={styles.numberOfPages}>{book?.number_of_pages} Pages</Text>
       </View>
     </TouchableOpacity>
   );
